5c5b2dfecefb8e5ec508a4008fefab2c378d6942
singhsavan <singhsavan@gmail.com>
2016-04-09 22:24:19 -0700

    convert address servics
    added the convert address service to convert the address into latitude and longitude

--- Geolocation/js/services/service.js
+++ Geolocation/js/services/service.js
@@ -1,4 +1,12 @@
 /*
+ * Service settings
+ */
+var world_places_settings = {
+    "database_url": "https://api.appery.io/rest/1/db",
+    "database_id": "5708bbaee4b0c3b227f0e480"
+}
+
+/*
  * Services
  */
 var GeolocationService = new Apperyio.GeolocationService({
@@ -11,5 +19,36 @@ var GeolocationService = new Apperyio.GeolocationService({
                 "watchPosition": false
             }
         }
+    }
+});
+
+var ConvertAddress = new Apperyio.RestService({
+    'url': 'https://maps.googleapis.com/maps/api/geocode/json',
+    'dataType': 'json',
+    'type': 'get',
+
+    'defaultRequest': {
+        "headers": {},
+        "parameters": {
+            "key": "AIzaSyBD8WIE1ibQWSi0jzIG-G2AgCEMs1Skaow"
+        },
+        "body": null
+    }
+});
+
+var world_places_Location_list_service = new Apperyio.RestService({
+    'url': '{database_url}/collections/Location',
+    'dataType': 'json',
+    'type': 'get',
+
+    'serviceSettings': world_places_settings
+
+    ,
+    'defaultRequest': {
+        "headers": {
+            "X-Appery-Database-Id": "{database_id}"
+        },
+        "parameters": {},
+        "body": null
     }
 });
